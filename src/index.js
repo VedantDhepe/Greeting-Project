0156324393a3913fe20cfb8762e5c711016c403c
Vedant Dhepe <vedantdhepe101@gmail.com>
2023-08-23 00:19:35 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var time = new Date();
+time = time.getHours();
+let wish;
+var col ={};
+if(time > 0 && time<12){
+  wish = "Good Morning";
+ col.color='green';
+}
+else if(time>12 && time<16){
+  wish = "Good Afternoon";
+  col.color='orange';
+}
+else{
+  wish = "Good Evening";
+  col.color='red';
+}
+ReactDOM.render(
+<div>
+<h1> Welcome, <span style={col}>{wish}</span></h1>
+</div>,
+document.getElementById('root'),);
