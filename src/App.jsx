ffeb2dac0fdbc89031823525a346e78ab379fba4
Vedant Dhepe <vedantdhepe101@gmail.com>
2023-08-23 20:41:44 +0530

    Components Added

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+function App() {
+  var time = new Date(); 
+    time = time.getHours();
+    let wish;
+    var col ={};
+    if(time > 0 && time<12){
+        wish = "Good Morning";
+        col.color='green';
+    }
+    else if(time>12 && time<16){
+        wish = "Good Afternoon";
+        col.color='orange';
+    }
+    else{
+        wish = "Good Evening";
+        col.color='red';
+    }
+  return(
+    <>
+   
+        <div>
+        <h1> Welcome Back, <span style={col}> {wish} </span></h1>
+        </div>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-var time = new Date();
-time = time.getHours();
-let wish;
-var col ={};
-if(time > 0 && time<12){
-  wish = "Good Morning";
- col.color='green';
-}
-else if(time>12 && time<16){
-  wish = "Good Afternoon";
-  col.color='orange';
-}
-else{
-  wish = "Good Evening";
-  col.color='red';
-}
+
 ReactDOM.render(
-<div>
-<h1> Welcome Back, <span style={col}> {wish} </span></h1>
-</div>,
+  <App></App>
+,
 document.getElementById('root'),);
